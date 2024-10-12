97e1408febc46a1bccbe8b0be26c945e43e86f03
NovikovaAlex <sashaulianova11@gmail.com>
2024-10-12 18:29:32 +0300

    Update лютый доп. блок 2.py

--- лютый доп. блок 2.py
+++ лютый доп. блок 2.py
@@ -3,16 +3,12 @@ x = int(input())
 key = []
 print('Ваш код: ')
 for i in range(1,20):
-    if i == 1:
-        is_prime = True
     for j in range(i,20):
         if x % (i + j) == 0 and i != j :
-            key.append(i)
-            if is_prime:
-                key.append(j)
-                continue
-
-print(int (x),':', *(key))
+            key.append(str(i))
+            key.append(str(j))
+            continue
+print(int(x),'-', ''.join( key))
 
 
 
