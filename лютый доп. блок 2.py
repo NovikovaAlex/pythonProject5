b2974766d492757051c9da167be218cdf230085d
NovikovaAlex <sashaulianova11@gmail.com>
2024-10-13 19:12:13 +0300

    Update лютый доп. блок 2.py

--- лютый доп. блок 2.py
+++ лютый доп. блок 2.py
@@ -2,13 +2,12 @@ print('Введите любое число от 3 до 20: ')
 x = int(input())
 key = []
 print('Ваш код: ')
-for i in range(1,20):
-    for j in range(i,20):
+for i in range(1,x):
+    for j in range(i,x):
         if x % (i + j) == 0 and i != j :
             key.append(str(i))
             key.append(str(j))
-            continue
-print(int(x),'-', ''.join( key))
+print(int(x),'-',*key, sep ='')
 
 
 
