dc721e9e9798fe2dd66c2c3fa8effc10a40dcc36
NovikovaAlex <sashaulianova11@gmail.com>
2024-10-13 19:34:20 +0300

    Update Пространство имён.py

--- Пространство имён.py
+++ Пространство имён.py
@@ -4,17 +4,17 @@ def count_calls():
     calls += 1
 
 def string_info (string):
-    print(len(string), (string.upper()), (string.lower()))
     count_calls()
+    return (len(string), (string.upper()), (string.lower()))
 
 def is_contains(string, list_to_search):
-    if string.upper() in (list(map(str.upper, list_to_search))):
-        print(True)
-    else:
-        print(False)
     count_calls()
+    if string.upper() in (list(map(str.upper, list_to_search))):
+        return True
+    else:
+        return False
 
-print(string_info('Capibara'))
+print(string_info('Capybara'))
 print(string_info('Armageddon'))
 print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
 print(is_contains('cycle', ['recycling', 'cyclic']))
