1a82a20840ee51f0c99b91aca462c2f3bc322ad2
NovikovaAlex <sashaulianova11@gmail.com>
2024-11-03 21:34:31 +0300

    Update Специальные методы классов.py

--- Специальные методы классов.py
+++ Специальные методы классов.py
@@ -4,7 +4,7 @@ class House:
         self.number_of_floor = number_of_floor
 
     def __str__(self):
-        return (f'Название: {self.name}, кол-во этажей: {self.number_of_floor}')
+        return f'Название: {self.name}, кол-во этажей: {self.number_of_floor}'
 
     def __len__(self):
         return self.number_of_floor
@@ -28,23 +28,38 @@ class House:
         return self.number_of_floor != other.number_of_floor
 
     def __add__(self, value):
-        self.number_of_floor += value
-        return (f'Название: {self.name}, кол-во этажей: {self.number_of_floor }')
+        if isinstance(value, int):
+            return House(self.name,  self.number_of_floor + value)
+        else:
+            return NotImplemented
 
-
-
-
+    def __radd__(self, value):
+        if isinstance(value, int):
+            return House(self.name, value + self.number_of_floor )
+        else:
+            return NotImplemented
 
 h1 = House('ЖК Эльбрус', 10)
 h2 = House('ЖК Акация', 20)
 print(h1)
 print(h2)
 print(h1 == h2)
-print(type(h1))
+
 h1 = h1 + 10
-print(type(h1))
+print(h1)
+print(h1 == h2)
+
+h1+=10
 print(h1)
 
+h2 = 10 + h2
+print(h2)
+
+print(h1 > h2)
+print(h1 >= h2)
+print(h1 < h2)
+print(h1 <= h2)
+print(h1 != h2)
 
 
 
