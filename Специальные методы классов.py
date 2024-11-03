81ac8bc8f7550213393f8f3ab63fd26890d2becc
NovikovaAlex <sashaulianova11@gmail.com>
2024-11-03 20:44:13 +0300

    Add files via upload

--- Специальные методы классов.py
+++ Специальные методы классов.py
@@ -9,10 +9,45 @@ class House:
     def __len__(self):
         return self.number_of_floor
 
+    def __eq__(self, other):
+        return self.number_of_floor == other.number_of_floor
+
+    def __it__(self, other):
+        return self.number_of_floor < other.number_of_floor
+
+    def __le__(self, other):
+        return self.number_of_floor <= other.number_of_floor
+
+    def __gt__(self, other):
+        return self.number_of_floor > other.number_of_floor
+
+    def __ge__(self, other):
+        return self.number_of_floor >= other.number_of_floor
+
+    def __ne__(self, other):
+        return self.number_of_floor != other.number_of_floor
+
+    def __add__(self, value):
+        new = House(self.x)
+        new.x = value.x
+        self.number_of_floor += value
+        return (f'Название: {self.name}, кол-во этажей: {self.number_of_floor }')
+
+
+
+
 
 h1 = House('ЖК Эльбрус', 10)
 h2 = House('ЖК Акация', 20)
 print(h1)
 print(h2)
-print(len(h1))
-print(len(h2))
+print(h1 == h2)
+print(type(h1))
+h1 = h1 + 10
+print(type(h1))
+print(h1)
+
+
+
+
+
