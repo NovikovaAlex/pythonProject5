20b43028c04ee6680fd159e9176c14ac4bc77821
NovikovaAlex <sashaulianova11@gmail.com>
2024-11-28 16:22:20 +0300

    Add files via upload

---
+++ Try и Except.py
@@ -0,0 +1,11 @@
+def add_everything_up(a,b):
+    try:
+        result = a + b
+        res = round(result,3)
+        return res
+    except TypeError:
+        return f'{a}{b}'
+
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
